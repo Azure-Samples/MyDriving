fd7abc92de2eadecbeff9187d4a2de13e39f6cfe
Nicole Haugen <nicolehaugen79@hotmail.com>
2016-02-18 10:32:30 -0600

    Checking in solution created using VS project template for Xamarin.Forms + PCL.  Note that we can use this same solution for creating Xamarin Native if needed.

---
+++ SmartKar/SmartKar.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SmartKar.WinPhone.Resources;
+
+namespace SmartKar.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
