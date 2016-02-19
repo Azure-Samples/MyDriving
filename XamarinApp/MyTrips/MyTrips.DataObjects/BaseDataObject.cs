0f1758d4b010a6d71cd9727787b0b4b6b97eb972
James Montemagno <james.montemagno@gmail.com>
2016-02-19 14:48:34 -0700

    Add new solution setup

---
+++ XamarinApp/MyTrips/MyTrips.DataObjects/BaseDataObject.cs
@@ -0,0 +1,38 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTrips.DataObjects
+{
+    public interface IBaseDataObject
+    {
+        string Id { get; set; }
+    }
+#if BACKEND
+    public class BaseDataObject : EntityData
+    {
+        public BaseDataObject ()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        public bool IsHidden { get; set; }
+    }
+#else
+    public class BaseDataObject : ObservableObject, IBaseDataObject
+    {
+        public BaseDataObject()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public bool IsHidden { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.Version]
+        public string AzureVersion { get; set; }
+    }
+#endif
+}
