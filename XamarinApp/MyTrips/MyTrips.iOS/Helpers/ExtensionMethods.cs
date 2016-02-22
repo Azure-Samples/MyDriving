207f720653f07eb899bf75420ee54fefe33355f5
Pierce Boggan <pierce.boggan@xamarin.com>
2016-02-22 13:52:55 -0500

    Added theme manager for easy app styling.

--- XamarinApp/MyTrips/MyTrips.iOS/Helpers/ExtensionMethods.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Helpers/ExtensionMethods.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using UIKit;
 
 namespace MyTrips.iOS
 {
-	public class ExtensionMethods
+	public static class ExtensionMethods
 	{
-		public ExtensionMethods ()
+		public static UIColor ToColor (this string hexValue)
 		{
+			if (hexValue.Contains ("#")) {
+				hexValue = hexValue.Replace("#", "");
+			}
+
+			if (hexValue.Length != 6) {
+				throw new ArgumentOutOfRangeException ("Hexadecimal values must be six characters long.");
+			}
+
+			int red = Int32.Parse (hexValue.Substring (0,2), System.Globalization.NumberStyles.AllowHexSpecifier);
+			int green = Int32.Parse (hexValue.Substring (2,2), System.Globalization.NumberStyles.AllowHexSpecifier);
+			int blue = Int32.Parse (hexValue.Substring (4,2), System.Globalization.NumberStyles.AllowHexSpecifier);
+
+			return UIColor.FromRGB (red, green, blue);
 		}
 	}
 }
