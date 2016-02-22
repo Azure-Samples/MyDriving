d0c05c6d9bdc6ac97d3b4b7872c7cbbd815004c5
Pierce Boggan <pierce.boggan@xamarin.com>
2016-02-22 15:01:21 -0500

    Add Current Trip view controller and map.

---
+++ XamarinApp/MyTrips/MyTrips.iOS/Screens/CurrentTripViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace MyTrips.iOS
+{
+	partial class CurrentTripViewController : UIViewController
+	{
+		public CurrentTripViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
