03f8f814e7c45baafdd3e31a62c19e24e27ebffa
Pierce Boggan <pierce.boggan@xamarin.com>
2016-02-23 16:59:45 -0500

    Scaffold code to update routes live.

--- XamarinApp/MyTrips/MyTrips.iOS/Screens/CurrentTripViewController.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Screens/CurrentTripViewController.cs
@@ -13,6 +13,8 @@ namespace MyTrips.iOS
 		CLLocationCoordinate2D locBoston = new CLLocationCoordinate2D(42.3601, -71.0589);
 		CLLocationManager locationManager = new CLLocationManager();
 
+		CarAnnotation currentLocationAnnotation;
+
 		public CurrentTripViewController (IntPtr handle) : base (handle)
 		{
 		}
@@ -22,17 +24,33 @@ namespace MyTrips.iOS
 			base.ViewDidLoad();
 
 			tripMapView.Delegate = new MapViewDelegate ();
-			tripMapView.AddAnnotations (new CarAnnotation (locBoston));
+
+			currentLocationAnnotation = new CarAnnotation(locBoston);
+			tripMapView.AddAnnotations(currentLocationAnnotation);
 
 			tripMapView.Camera.CenterCoordinate = new CLLocationCoordinate2D(37.797534, -122.401827);
 			tripMapView.Camera.Altitude = 5000;
 
 			// TODO: Add a graceful way to handle permission issues.
 			if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0) == true) {
-				locationManager.RequestAlwaysAuthorization ();
+				locationManager.RequestAlwaysAuthorization();
 			}
 
 			tripMapView.ShowsUserLocation = true;
+		}
+
+		public void UserLocationChanged(CLLocationCoordinate2D newLocation)
+		{
+			// Update annotations
+			tripMapView.RemoveAnnotation(currentLocationAnnotation);
+
+			currentLocationAnnotation = new CarAnnotation(newLocation);
+
+			tripMapView.AddAnnotation(currentLocationAnnotation);
+
+			// Move map camera
+			tripMapView.Camera.CenterCoordinate = newLocation;
+			tripMapView.Camera.Altitude = 5000;
 		}
 	}
 }
