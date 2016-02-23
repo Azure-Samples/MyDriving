daccbbe6d82f5ecbcf620cae2ca78450aa23dae9
Pierce Boggan <pierce.boggan@xamarin.com>
2016-02-23 13:04:19 -0500

    Abstract custom annotations out to a base class.

--- XamarinApp/MyTrips/MyTrips.iOS/Views/CarAnnotation.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Views/CarAnnotation.cs
@@ -1,26 +1,11 @@
 ﻿using System;
-
-using Foundation;
-using UIKit;
-using MapKit;
 using CoreLocation;
-
 namespace MyTrips.iOS
 {
-	public class CarAnnotation : MKAnnotation
+	public class CarAnnotation : BaseCustomAnnotation
 	{
-		CLLocationCoordinate2D coordinate;
-
-		public CarAnnotation(CLLocationCoordinate2D annotationLocation)
-		{
-			coordinate = annotationLocation;
-		}
-
-		public override CLLocationCoordinate2D Coordinate {
-			get {
-				return coordinate;
-			}
-		}
+		public CarAnnotation(CLLocationCoordinate2D annotationLocation) : base(annotationLocation)
+		{ }
 	}
 }
 
