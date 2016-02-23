8f4d08def215c39ed79f722dcabf8c9cbba8302f
Mike James <mike@xamarin.com>
2016-02-23 13:47:00 +0000

    Adding code behind for Trip view cell

--- XamarinApp/MyTrips/MyTrips.iOS/Cells/TripTableViewCell.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Cells/TripTableViewCell.cs
@@ -15,5 +15,41 @@ namespace MyTrips.iOS
 		{
 		}
 
+		public string LocationName
+		{
+			get
+			{
+				return lblTitle.Text;
+			}
+			set
+			{
+				lblTitle.Text = value;
+			}
+		}
+
+		public string TimeAgo
+		{
+			get
+			{
+				return lblDaysAgo.Text;
+			}
+			set
+			{
+				lblDaysAgo.Text = value;
+			}
+		}
+
+		public string Distance
+		{
+			get
+			{
+				return lblMiles.Text;
+			}
+			set
+			{
+				lblMiles.Text = value;
+			}
+		}
+
     }
 }
--- XamarinApp/MyTrips/MyTrips.iOS/Cells/TripTableViewCell.designer.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Cells/TripTableViewCell.designer.cs
@@ -14,8 +14,34 @@ namespace MyTrips.iOS
     [Register ("TripTableViewCell")]
     partial class TripTableViewCell
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblDaysAgo { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblMiles { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblTitle { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (lblDaysAgo != null) {
+                lblDaysAgo.Dispose ();
+                lblDaysAgo = null;
+            }
+
+            if (lblMiles != null) {
+                lblMiles.Dispose ();
+                lblMiles = null;
+            }
+
+            if (lblTitle != null) {
+                lblTitle.Dispose ();
+                lblTitle = null;
+            }
         }
     }
 }
