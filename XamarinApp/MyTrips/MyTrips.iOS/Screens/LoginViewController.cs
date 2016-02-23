46d29fac5754ec50716ff592f9e45daef6176341
Mike James <mike@xamarin.com>
2016-02-23 12:38:55 +0000

    Implemented login buttons fading in.

--- XamarinApp/MyTrips/MyTrips.iOS/Screens/LoginViewController.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Screens/LoginViewController.cs
@@ -12,6 +12,36 @@ namespace MyTrips.iOS
 			
 		}
 
+		public override void ViewDidLoad()
+		{
+			//Prepare buttons for fade in animation.
+			btnFacebook.Alpha = 0;
+			btnTwitter.Alpha = 0;
+			btnMicrosoft.Alpha = 0;
+		}
 
+		public override void ViewDidAppear(bool animated)
+		{
+			base.ViewDidAppear(animated);
+
+			btnFacebook.FadeIn(0.2, 0.5f);
+			btnTwitter.FadeIn(0.2, 0.7f);
+			btnMicrosoft.FadeIn(0.2, 1);
+		}
+
+		partial void BtnFacebook_TouchUpInside(UIButton sender)
+		{
+			throw new NotImplementedException();
+		}
+
+		partial void BtnTwitter_TouchUpInside(UIButton sender)
+		{
+			throw new NotImplementedException();
+		}
+
+		partial void BtnMicrosoft_TouchUpInside(UIButton sender)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
--- XamarinApp/MyTrips/MyTrips.iOS/Screens/LoginViewController.designer.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Screens/LoginViewController.designer.cs
@@ -11,11 +11,58 @@ using UIKit;
 
 namespace MyTrips.iOS
 {
-	[Register ("LoginViewController")]
-	partial class LoginViewController
-	{
-		void ReleaseDesignerOutlets ()
-		{
-		}
-	}
+    [Register ("LoginViewController")]
+    partial class LoginViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnFacebook { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnMicrosoft { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnTwitter { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView imgLogo { get; set; }
+
+        [Action ("BtnFacebook_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnFacebook_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("BtnTwitter_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnTwitter_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("BtnMicrosoft_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnMicrosoft_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnFacebook != null) {
+                btnFacebook.Dispose ();
+                btnFacebook = null;
+            }
+
+            if (btnMicrosoft != null) {
+                btnMicrosoft.Dispose ();
+                btnMicrosoft = null;
+            }
+
+            if (btnTwitter != null) {
+                btnTwitter.Dispose ();
+                btnTwitter = null;
+            }
+
+            if (imgLogo != null) {
+                imgLogo.Dispose ();
+                imgLogo = null;
+            }
+        }
+    }
 }
