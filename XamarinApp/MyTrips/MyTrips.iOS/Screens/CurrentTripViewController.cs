999b907639345288682e948a7df1011ec86376cd
Pierce Boggan <pierce.boggan@xamarin.com>
2016-02-23 14:00:14 -0500

    Request permission to track user's location.

--- XamarinApp/MyTrips/MyTrips.iOS/Screens/CurrentTripViewController.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Screens/CurrentTripViewController.cs
@@ -11,6 +11,7 @@ namespace MyTrips.iOS
 	partial class CurrentTripViewController : UIViewController
 	{
 		CLLocationCoordinate2D locBoston = new CLLocationCoordinate2D(42.3601, -71.0589);
+		CLLocationManager locationManager = new CLLocationManager();
 
 		public CurrentTripViewController (IntPtr handle) : base (handle)
 		{
@@ -25,6 +26,13 @@ namespace MyTrips.iOS
 
 			tripMapView.Camera.CenterCoordinate = new CLLocationCoordinate2D(37.797534, -122.401827);
 			tripMapView.Camera.Altitude = 5000;
+
+			// TODO: Add a graceful way to handle permission issues.
+			if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0) == true) {
+				locationManager.RequestAlwaysAuthorization ();
+			}
+
+			tripMapView.ShowsUserLocation = true;
 		}
 	}
 }
