97e2dc88910958d90ca6b1f8796920070896b093
Mike James <mike@xamarin.com>
2016-02-23 12:27:36 +0000

    Created login controller

---
+++ XamarinApp/MyTrips/MyTrips.iOS/Screens/LoginViewController.cs
@@ -0,0 +1,17 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace MyTrips.iOS
+{
+	partial class LoginViewController : UIViewController
+	{
+		public LoginViewController (IntPtr handle) : base (handle)
+		{
+			
+		}
+
+
+	}
+}
