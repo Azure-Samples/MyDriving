171f399cb64622f2dfce4d863a77d80eac11fdb0
James Montemagno <james.montemagno@gmail.com>
2016-02-23 20:13:17 -0700

    Add initial fragments

---
+++ XamarinApp/MyTrips/MyTrips.Android/Fragments/FragmentPastTrips.cs
@@ -0,0 +1,30 @@
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+
+
+namespace MyTrips.Droid.Fragments
+{
+    public class FragmentPastTrips : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public static Fragment1 NewInstance()
+        {
+            var frag1 = new Fragment1 { Arguments = new Bundle() };
+            return frag1;
+        }
+
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var ignored = base.OnCreateView(inflater, container, savedInstanceState);
+            return inflater.Inflate(Resource.Layout.fragment_past_trips, null);
+        }
+    }
+}
