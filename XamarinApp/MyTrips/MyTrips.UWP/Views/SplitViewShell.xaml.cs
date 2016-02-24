dabd742820a137043cc505ea06f813f9b1c85621
Nicole Haugen <nicolehaugen79@hotmail.com>
2016-02-24 17:16:05 -0600

    Adding missed files

---
+++ XamarinApp/MyTrips/MyTrips.UWP/Views/SplitViewShell.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MyTrips.UWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SplitViewShell : Page
+    {
+        public SplitViewShell(Frame frame)
+        {
+            this.InitializeComponent();
+            this.MyTripsSplitView.Content = frame;
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyTripsSplitView.IsPaneOpen = !MyTripsSplitView.IsPaneOpen;
+        }
+
+        private void RecommendedRoutesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyTripsSplitView.IsPaneOpen = false;
+            ((Frame)this.MyTripsSplitView.Content).Navigate(typeof(RecommendedRoutes));
+        }
+
+        private void TripsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyTripsSplitView.IsPaneOpen = false;
+            ((Frame)this.MyTripsSplitView.Content).Navigate(typeof(PastTripsMenuView));
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyTripsSplitView.IsPaneOpen = false;
+            ((Frame)this.MyTripsSplitView.Content).Navigate(typeof(Settings));
+        }
+    }
+}
