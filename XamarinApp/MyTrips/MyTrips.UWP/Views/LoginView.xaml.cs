f336b3ca536bb69f5ddc65a50c8d8645b6ec5473
Alina Popa <alpopa@microsoft.com>
2016-02-25 23:16:37 -0800

    Fix merge error

--- XamarinApp/MyTrips/MyTrips.UWP/Views/LoginView.xaml.cs
+++ XamarinApp/MyTrips/MyTrips.UWP/Views/LoginView.xaml.cs
@@ -16,6 +16,9 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices;
 using Windows.UI.Popups;
 using MyTrips.ViewModel;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -68,5 +71,67 @@ namespace MyTrips.UWP.Views
             Window.Current.Content = new SplitViewShell(this.Frame);
             this.Frame.Navigate(typeof(PastTripsMenuView));
         }
+
+        private async void FaceBookButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Login the user and then load data from the mobile app.
+            if (await AuthenticateWFacebookAsync())
+            {
+                LoginButtons.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+
+                WelcomeText.Text = "Welcome. UserID = " + user.UserId;
+
+                //await RefreshTodoItems();
+            }
+            else
+            {
+                WelcomeText.Text = "Login failed";
+            }
+            WelcomeText.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        }
+
+        // Define a member variable for storing the signed-in user. 
+        private MobileServiceUser user;
+
+        // Define a method that performs the authentication process
+        // using a Facebook sign-in. 
+        private async System.Threading.Tasks.Task<bool> AuthenticateWFacebookAsync()
+        {
+
+            bool success = false;
+            try
+            {
+                // Sign-in using Facebook authentication.
+                user = await App.MobileService
+                    .LoginAsync(MobileServiceAuthenticationProvider.Facebook);
+
+                success = true;
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+            return success;
+        }
+
+        //public async Task<string> GetFacebookUserDataAsync()
+        //{
+        //    HttpClient httpClient = CreateHttpClient();
+
+        //    //string url = "https://graph.facebook.com/v2.5/me?fields=first_name&access_token=" + accessToken;
+          
+
+        //    string url = App.MobileAppsUrl + "/tables";
+        //    var response = await httpClient.GetStringAsync(url);
+        //    return response;
+        //}
+
+        //private HttpClient CreateHttpClient()
+        //{
+        //    var httpClient = new HttpClient();
+        //    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //    return httpClient;
+        //}
+
     }
 }
