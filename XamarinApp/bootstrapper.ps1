6b7fef051c8a8078fd06ba7a13e17a1d4faccd24
James Montemagno <james.montemagno@gmail.com>
2016-02-25 16:08:46 -0800

    update cake script

--- XamarinApp/bootstrapper.ps1
+++ XamarinApp/bootstrapper.ps1
@@ -1,5 +1,5 @@
 Param(
-    [string]$Script = "build.cake",
+    [string]$Script = "XamarinApp/build.cake",
     [string]$Target = "Default",
     [string]$Configuration = "Release",
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
