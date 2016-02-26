36b7cc3f68bc5dc4f8593d48cf14d0cf10a7ed8d
Mike James <mike@xamarin.com>
2016-02-26 13:53:45 +0000

    Improved the colours throughout the app

--- XamarinApp/MyTrips/MyTrips.iOS/Helpers/ThemeManager.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Helpers/ThemeManager.cs
@@ -13,15 +13,25 @@ namespace MyTrips.iOS
 
 			// Navigation Bar
 			UINavigationBar.Appearance.BarTintColor = Colors.PRIMARY_DARK;
+			UINavigationBar.Appearance.TintColor = Colors.PRIMARY;
 			UINavigationBar.Appearance.SetTitleTextAttributes (new UITextAttributes {
 				Font = UIFont.FromName("AvenirNext-Medium", 17),
 				TextColor = Colors.PRIMARY
 			});
 
+			UIBarButtonItem.Appearance.SetTitleTextAttributes (new UITextAttributes {
+				Font = UIFont.FromName("AvenirNext-Medium", 15),
+				TextColor = Colors.PRIMARY,
+
+			}, UIControlState.Normal);
+
+
 			UITabBarItem.Appearance.SetTitleTextAttributes (new UITextAttributes {
 				Font = UIFont.FromName("AvenirNext", 15),
 				TextColor = Colors.PRIMARY.Lighten(5)
 			}, UIControlState.Normal);
+
+
 
 			//UITabBar.Appearance.TintColor = PRIMARY;
 
