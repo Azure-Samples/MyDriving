10f095837711ab5874a2e138b0b52d363d13c85d
Pierce Boggan <pierce.boggan@xamarin.com>
2016-02-26 00:00:19 -0500

    [iOS] Refactor colors out to own constants class.

--- XamarinApp/MyTrips/MyTrips.iOS/Helpers/ExtensionMethods.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Helpers/ExtensionMethods.cs
@@ -10,23 +10,6 @@ namespace MyTrips.iOS
 {
 	public static class ExtensionMethods
 	{
-		public static UIColor ToColor (this string hexValue)
-		{
-			if (hexValue.Contains ("#")) {
-				hexValue = hexValue.Replace("#", "");
-			}
-
-			if (hexValue.Length != 6) {
-				throw new ArgumentOutOfRangeException ("Hexadecimal values must be six characters long.");
-			}
-
-			int red = Int32.Parse (hexValue.Substring (0,2), System.Globalization.NumberStyles.AllowHexSpecifier);
-			int green = Int32.Parse (hexValue.Substring (2,2), System.Globalization.NumberStyles.AllowHexSpecifier);
-			int blue = Int32.Parse (hexValue.Substring (4,2), System.Globalization.NumberStyles.AllowHexSpecifier);
-
-			return UIColor.FromRGB (red, green, blue);
-		}
-
 		#region Animations 
 		public enum UIViewAnimationFlipDirection
 		{
--- XamarinApp/MyTrips/MyTrips.iOS/Helpers/ThemeManager.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Helpers/ThemeManager.cs
@@ -5,15 +5,6 @@ namespace MyTrips.iOS
 {
 	public class ThemeManager
 	{
-		static UIColor PRIMARY = "607DB8".ToColor ();
-		static UIColor PRIMARY_DARK = "111820".ToColor ();
-	    static UIColor PRIMARY_LIGHT = "CFD8DC".ToColor ();
-		static UIColor ACCENT = "FF4081".ToColor ();
-		static UIColor PRIMARY_TEXT = "212121".ToColor ();
-		static UIColor SECONDARY_TEXT = "727272".ToColor ();
-		static UIColor ICONS = "FFFFFF".ToColor ();
-		static UIColor DIVIDER = "B6B6B6".ToColor ();
-
 		public static void ApplyTheme ()
 		{
 			
@@ -21,15 +12,15 @@ namespace MyTrips.iOS
 			UIApplication.SharedApplication.StatusBarStyle = UIStatusBarStyle.LightContent;
 
 			// Navigation Bar
-			UINavigationBar.Appearance.BarTintColor = PRIMARY_DARK;
+			UINavigationBar.Appearance.BarTintColor = Colors.PRIMARY_DARK;
 			UINavigationBar.Appearance.SetTitleTextAttributes (new UITextAttributes {
 				Font = UIFont.FromName("AvenirNext-Medium", 17),
-				TextColor = PRIMARY
+				TextColor = Colors.PRIMARY
 			});
 
 			UITabBarItem.Appearance.SetTitleTextAttributes (new UITextAttributes {
 				Font = UIFont.FromName("AvenirNext", 15),
-				TextColor = PRIMARY.Lighten(5)
+				TextColor = Colors.PRIMARY.Lighten(5)
 			}, UIControlState.Normal);
 
 			//UITabBar.Appearance.TintColor = PRIMARY;
