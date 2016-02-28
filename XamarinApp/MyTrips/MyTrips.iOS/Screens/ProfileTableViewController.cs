9727c5e00c10a40e5618d10d2bd97fe425e69329
Mike James <mike@xamarin.com>
2016-02-28 11:07:35 +0000

    Adding attributed text for profile percentage.

--- XamarinApp/MyTrips/MyTrips.iOS/Screens/ProfileTableViewController.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Screens/ProfileTableViewController.cs
@@ -9,5 +9,32 @@ namespace MyTrips.iOS
         public ProfileTableViewController (IntPtr handle) : base (handle)
         {
         }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			//UpdateHeader(64);
+		}
+
+		void UpdateHeader(int percentage)
+		{
+			lblPercentage.Text = $"{percentage}%";
+			lblBetterThan.Text = $"Better driver than {percentage} of Americans";
+
+			var defaultAttributes = new UIStringAttributes {
+				ForegroundColor = lblBetterThan.TextColor,
+			};
+
+			var hitAttributes = new UIStringAttributes {
+				ForegroundColor = Colors.PRIMARY,
+			};
+
+			var attributedString = new NSMutableAttributedString (lblBetterThan.Text);
+			attributedString.SetAttributes(defaultAttributes.Dictionary, new NSRange(0, lblBetterThan.Text.Length));
+			attributedString.SetAttributes(hitAttributes.Dictionary, new NSRange(15, lblBetterThan.Text.Length));
+
+			lblBetterThan.AttributedText = attributedString;
+		}
     }
 }
