78dbbf817532da1a2c1bb2b535bbcc360395b0a0
Mike James <mike@xamarin.com>
2016-02-28 10:53:19 +0000

    Added Profile Page

---
+++ XamarinApp/MyTrips/MyTrips.iOS/Screens/ProfileTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MyTrips.iOS
+{
+    public partial class ProfileTableViewController : UITableViewController
+    {
+        public ProfileTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
