a6b033f60e251f25776cfbd7e9bc5994aef2e94b
Alina Popa <alpopa@microsoft.com>
2016-02-29 19:26:10 -0800

    Add a horizontal title bar to the SplitViewShell UI

--- XamarinApp/MyTrips/MyTrips.UWP/Views/SettingsView.xaml.cs
+++ XamarinApp/MyTrips/MyTrips.UWP/Views/SettingsView.xaml.cs
@@ -20,9 +20,9 @@ namespace MyTrips.UWP.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Settings : Page
+    public sealed partial class SettingsView : Page
     {
-        public Settings()
+        public SettingsView()
         {
             this.InitializeComponent();
         }
