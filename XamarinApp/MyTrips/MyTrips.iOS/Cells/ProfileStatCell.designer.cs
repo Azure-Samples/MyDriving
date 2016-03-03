7ef2ac87eb7aa7dfae38777e33d7f5e90ea6d509
Pierce Boggan <pierce.boggan@xamarin.com>
2016-03-03 14:16:05 -0500

    [iOS] Fixed profile cell issue.

--- XamarinApp/MyTrips/MyTrips.iOS/Cells/ProfileStatCell.designer.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Cells/ProfileStatCell.designer.cs
@@ -16,14 +16,27 @@ namespace MyTrips.iOS
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView imgIcon { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UILabel lblStatName { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UILabel lblText { get; set; }
 
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView sideBox { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (imgIcon != null) {
+                imgIcon.Dispose ();
+                imgIcon = null;
+            }
+
             if (lblStatName != null) {
                 lblStatName.Dispose ();
                 lblStatName = null;
@@ -32,6 +45,11 @@ namespace MyTrips.iOS
             if (lblText != null) {
                 lblText.Dispose ();
                 lblText = null;
+            }
+
+            if (sideBox != null) {
+                sideBox.Dispose ();
+                sideBox = null;
             }
         }
     }
