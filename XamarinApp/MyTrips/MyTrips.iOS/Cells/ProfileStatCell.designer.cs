3f53662ef8541f56e967d17e39e4d4e0dd913d01
Mike James <mike@xamarin.com>
2016-03-03 17:29:15 +0000

    Fixing profile

--- XamarinApp/MyTrips/MyTrips.iOS/Cells/ProfileStatCell.designer.cs
+++ XamarinApp/MyTrips/MyTrips.iOS/Cells/ProfileStatCell.designer.cs
@@ -16,27 +16,14 @@ namespace MyTrips.iOS
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIImageView imgIcon { get; set; }
-
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UILabel lblStatName { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UILabel lblText { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIView sideBox { get; set; }
-
         void ReleaseDesignerOutlets ()
         {
-            if (imgIcon != null) {
-                imgIcon.Dispose ();
-                imgIcon = null;
-            }
-
             if (lblStatName != null) {
                 lblStatName.Dispose ();
                 lblStatName = null;
@@ -45,11 +32,6 @@ namespace MyTrips.iOS
             if (lblText != null) {
                 lblText.Dispose ();
                 lblText = null;
-            }
-
-            if (sideBox != null) {
-                sideBox.Dispose ();
-                sideBox = null;
             }
         }
     }
