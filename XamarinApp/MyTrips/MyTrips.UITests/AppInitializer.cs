cfe3d65088a15953670d78cdf4966b9fa04c2f2d
Pierce Boggan <pierce.boggan@xamarin.com>
2016-03-11 16:07:06 -0500

    [Test] Add helper UITest classes.

--- XamarinApp/MyTrips/MyTrips.UITests/AppInitializer.cs
+++ XamarinApp/MyTrips/MyTrips.UITests/AppInitializer.cs
@@ -8,19 +8,40 @@ namespace MyTrips.UITests
 {
     public class AppInitializer
     {
+		// const string apkPath = "../../../com.xamarin.samples.taskydroid-Signed.apk";
+		// const string appPath = "../../../TaskyiOS.app";
+		// const string ipaBundleId = "com.xamarin.samples.taskytouch";
+
+		private static IApp app;
+		public static IApp App
+		{
+			get
+			{
+				if (app == null)
+					throw new NullReferenceException("'AppInitializer.App' not set. Call 'AppInitializer.StartApp(platform)' before trying to access it.");
+				return app;
+			}
+		}
+
         public static IApp StartApp(Platform platform)
         {
-            if (platform == Platform.Android)
-            {
-                return ConfigureApp
-                    .Android
-                    .StartApp();
-            }
+			if (platform == Platform.Android)
+			{
+				app = ConfigureApp
+					.Android
+					// .ApkFile(apkPath)
+					.StartApp();
+			}
+			else
+			{
+				app = ConfigureApp
+					.iOS
+					// .AppBundle(appPath)
+					// .InstalledApp(ipaBundleId)
+					.StartApp();
+			}
 
-            return ConfigureApp
-                .iOS
-                .StartApp();
+			return app;
         }
     }
 }
-
