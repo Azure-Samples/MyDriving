cfa16eed42a9280f02fe32259cb1fc10451d258a
Pierce Boggan <pierce.boggan@xamarin.com>
2016-03-14 13:57:45 -0400

    [Test] Added test setup helper.

---
+++ XamarinApp/MyTrips/MyTrips.UITests/AbstractSetup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Xamarin.UITest;
+using NUnit.Framework;
+using System.Linq;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.iOS;
+
+namespace MyTrips.UITests
+{
+	[TestFixture(Platform.Android)]
+	[TestFixture(Platform.iOS)]
+	public abstract class AbstractSetup
+	{
+		protected IApp app;
+		protected Platform platform;
+		protected bool OnAndroid;
+		protected bool OniOS;
+
+		public AbstractSetup(Platform platform)
+		{
+			this.platform = platform;
+		}
+
+		[SetUp]
+		public virtual void BeforeEachTest()
+		{
+			app = AppInitializer.StartApp(platform);
+			OnAndroid = app.GetType() == typeof(AndroidApp);
+			OniOS = app.GetType() == typeof(iOSApp);
+
+			if (app.Query("LoginPageIdentifier").Any())
+			{
+				new LoginPage ()
+					.SkipAuthentication ();
+				
+				Thread.Sleep(2000);
+			}
+		}
+
+		public void ClearKeychain ()
+		{
+			app.TestServer.Post("/keychain", new object());
+			app = ConfigureApp.iOS.StartApp();
+		}
+	}
+}
