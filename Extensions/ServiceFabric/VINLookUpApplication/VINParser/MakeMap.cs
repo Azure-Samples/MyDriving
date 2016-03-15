1b2fe997aa2b48097630bff19adafc5254c8f903
HaishiBai <hbai@microsoft.com>
2016-03-15 16:19:26 -0700

    Finished VIN Lookup

---
+++ Extensions/ServiceFabric/VINLookUpApplication/VINParser/MakeMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VINParser
+{
+    public class MakeMap
+    {
+        public int StartDigit { get; set; }
+        public int MaxLength { get; set; }
+        public Dictionary<string,CarInfo> Map { get; set; }
+    }
+}
