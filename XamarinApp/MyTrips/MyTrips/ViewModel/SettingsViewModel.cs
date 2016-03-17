d6bd48f848b63ff1536c9fa2ff366a2fc7f3bce3
James Montemagno <james.montemagno@gmail.com>
2016-03-17 16:56:28 -0400

    Update urls

--- XamarinApp/MyTrips/MyTrips/ViewModel/SettingsViewModel.cs
+++ XamarinApp/MyTrips/MyTrips/ViewModel/SettingsViewModel.cs
@@ -17,12 +17,11 @@ namespace MyTrips.ViewModel
         //Use Settings.DeviceConnectionString
         public string PrivacyPolicyUrl => "http://microsoft.com";
         public string TermsOfUseUrl => "http://microsoft.com";
-        public string OpenSourceNoticeUrl =>"http://microsoft.com";
-        public string SourceOnGitHubUrl => "http://microsoft.com";
+        public string OpenSourceNoticeUrl =>"https://github.com/Azure-Samples/MyDriving/tree/master/MobileApp/Licenses";
+        public string SourceOnGitHubUrl => "https://github.com/Azure-Samples/MyDriving";
         public string XamarinUrl => "http://xamarin.com";
 
 		List<Setting> units;
-		List<Setting> ioTHub;
 		List<Setting> permissions;
 		List<Setting> about;
 
