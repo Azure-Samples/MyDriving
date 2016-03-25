c351e5bd9a81cba0d2ee0a91ed871273a4b7d7b3
Pierce Boggan <pierce.boggan@xamarin.com>
2016-03-25 14:48:42 -0400

    [iOS] Fix permissions denied issue.

--- src/MobileApps/MyDriving/MyDriving.iOS/Screens/Trips/CurrentTripViewController.cs
+++ src/MobileApps/MyDriving/MyDriving.iOS/Screens/Trips/CurrentTripViewController.cs
@@ -47,14 +47,19 @@ namespace MyDriving.iOS
             base.ViewDidAppear(animated);
             PopRecordButtonAnimation();
 
-			if (CurrentTripViewModel != null && !CurrentTripViewModel.Geolocator.IsListening)
+			if (CurrentTripViewModel != null)
 			{
 				await CurrentTripViewModel.ExecuteStartTrackingTripCommandAsync().ContinueWith(async (task) =>
 				{
 					// If we don't have permission from the user, prompt a dialog requesting permission.
-					if (!CurrentTripViewModel.Geolocator.IsGeolocationEnabled)
-						await PromptPermissionsChangeDialog();
+					await PromptPermissionsChangeDialog();
 				});
+
+				if (!CurrentTripViewModel.Geolocator.IsGeolocationEnabled)
+				{
+					tripMapView.Camera.CenterCoordinate = new CLLocationCoordinate2D(47.6204, -122.3491);
+					tripMapView.Camera.Altitude = 5000;
+				}
 			}
         }
 
