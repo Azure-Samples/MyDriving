d9abdefa1e56da1c3a4f8d5aeda5a1bfb5b6b265
Alina Popa <alpopa@microsoft.com>
2016-05-26 13:03:09 -0700

    Run LoginAsync on the UI thread

--- src/MobileApps/MyDriving/MyDriving.UWP/Helpers/Authentication.cs
+++ src/MobileApps/MyDriving/MyDriving.UWP/Helpers/Authentication.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices;
 using MyDriving.Utils;
 using MyDriving.Utils.Interfaces;
+using Windows.UI.Core;
 
 namespace MyDriving.UWP.Helpers
 {
@@ -18,14 +19,32 @@ namespace MyDriving.UWP.Helpers
         public async Task<MobileServiceUser> LoginAsync(IMobileServiceClient client,
             MobileServiceAuthenticationProvider provider)
         {
+            var coreWindow = Windows.ApplicationModel.Core.CoreApplication.MainView;
+            // Dispatcher needed to run on UI Thread
+            var dispatcher = coreWindow.CoreWindow.Dispatcher;
+
+            MobileServiceUser user = null;
+
+
             try
             {
-                var user = await client.LoginAsync(provider);
+                if (Settings.Current.IsLoggedIn)  //relogin
+                {
+                    await dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                    {
+                        user = await client.LoginAsync(provider);
 
-                Settings.Current.AuthToken = user?.MobileServiceAuthenticationToken ?? string.Empty;
-                Settings.Current.AzureMobileUserId = user?.UserId ?? string.Empty;
+                    });
+                }
+                else
+                    user = await client.LoginAsync(provider);
 
-                return user;
+                if (user != null)
+                {
+                    Settings.Current.AuthToken = user?.MobileServiceAuthenticationToken ?? string.Empty;
+                    Settings.Current.AzureMobileUserId = user?.UserId ?? string.Empty;
+                }
+
             }
             catch (Exception e)
             {
@@ -36,7 +55,7 @@ namespace MyDriving.UWP.Helpers
                 }
             }
 
-            return null;
+            return user;
         }
     }
 }
