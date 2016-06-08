92744bc26ee72f1f4f857cf66de4fca895a2040f
Karl Krukow <karl.krukow@xamarin.com>
2016-06-08 19:48:42 +0200

    Add sample scaffold ExampleTest

---
+++ src/MobileApps/MyDriving/MyDriving.UITests/Tests/ExampleTests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+using Xamarin.UITest;
+using NUnit.Framework;
+
+namespace MyDriving.UITests
+{
+	[TestFixture(Platform.Android)]
+	public class ExampleTests 
+	{
+		protected IApp app;
+		protected Platform platform;
+		public ExampleTests (Platform platform)
+		{
+			this.platform = platform;
+			app = AppInitializer.StartApp(platform);
+		}
+
+
+
+
+	}
+}
